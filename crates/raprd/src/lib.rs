36e6c168fd388fb769bda1cee402e80ac06dbddc
jaketakula <jake.zhang.work@gmail.com>
2025-07-19 18:24:25 +1000

    chore: use logo.svg in doc readme

--- crates/raprd/src/lib.rs
+++ crates/raprd/src/lib.rs
@@ -1,10 +1,9 @@
-// - Includes the README content in the generated documentation (rustdoc)
-// - Allows running doc tests from the README examples
-// - Makes the README content available through cargo doc
-// - Enables IDE documentation preview features
 #![doc = include_str!("../README.md")]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
-// #![doc(html_logo_url = "assets/icon.png", html_favicon_url = "assets/icon.png")]
+#![doc(
+    html_logo_url = "assets/logo.png",
+    html_favicon_url = "assets/logo.png"
+)]
 #![allow(missing_docs)]
 #![allow(dead_code)]
 
